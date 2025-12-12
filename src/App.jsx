d853e7cf0961d5c598cbeff5dbd51ad18008986d
dwhaling <dwhaling@ualberta.ca>
2025-12-11 17:36:19 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>My Book Catalog</h1>
+
+      <form>
+        <input type="text" placeholder="Book Title" />
+        <input type="text" placeholder="Author" />
+        <button>Add Book</button>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
+
