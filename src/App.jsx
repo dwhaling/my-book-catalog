0366bad96466d318361e15509af4f8ea67036c64
dwhaling <dwhaling@ualberta.ca>
2025-12-18 15:23:21 -0700

    Update App.jsx
    Added functionality to add/delete books, books also save to local storage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,78 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [books, setBooks] = useState(() => {
+    const storedBooks = localStorage.getItem("books");
+    return storedBooks ? JSON.parse(storedBooks) : [];
+  });
+
+  // save books to localStorage whenever 'books' state changes
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]); // run useEffect whenever 'books' changes
+
+
+  function handleSubmit(event) {
+    event.preventDefault(); // tells browser to NOT perform the default submit behavior.
+
+    if (title.trim() === "" || author.trim() === "") {
+      return;
+    }
+
+    const newBook = {
+      title: title,
+      author: author,
+    };
+
+    setBooks([...books, newBook]);
+
+    setTitle("");
+    setAuthor("");
+  }
+
+  function handleDeleteBook(indexToDelete) {
+    const updatedBooks = books.filter((_, index) => index !== indexToDelete);
+    setBooks(updatedBooks);
+  }
+
+
   return (
     <div>
       <h1>My Book Catalog</h1>
 
-      <form>
-        <input type="text" placeholder="Book Title" />
-        <input type="text" placeholder="Author" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+
         <button>Add Book</button>
       </form>
+
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            {book.title} — {book.author}
+            <button onClick={() => handleDeleteBook(index)}>🗑️</button>
+          </li>
+        ))}
+      </ul>
 
     </div>
   );
 }
 
 export default App;
+
 
